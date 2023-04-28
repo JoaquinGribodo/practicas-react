759addfb61577bb5e2db43a5053fdbc280a38f62
Joaquin Gribodo <joacogribodo@gmail.com>
2023-04-28 10:40:55 -0300

    AgregarLectura

--- src/components/BookForm/BookForm.js
+++ src/components/BookForm/BookForm.js
@@ -5,6 +5,7 @@ const BookForm = ({ onBookDataSaved }) => {
   const [enteredAuthor, setEnteredAuthor] = useState("");
   const [enteredPageCount, setPageCount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
+  const [showForm, setShowForm] = useState(false);
 
   const changeTitleHandler = (event) => {
     setEnteredTitle(event.target.value);
@@ -34,49 +35,64 @@ const BookForm = ({ onBookDataSaved }) => {
   };
 
   return (
-    <form onSubmit={submitBookHandler}>
-      <div className="new-book-controls">
-        <div className="new-book-control">
-          <label>Título </label>
-          <input
-            type="text"
-            onChange={changeTitleHandler}
-            value={enteredTitle}
-          ></input>
+    <>
+      {showForm ? (
+        <form onSubmit={submitBookHandler}>
+          <div className="new-book-controls">
+            <div className="new-book-control">
+              <label>Título </label>
+              <input
+                type="text"
+                onChange={changeTitleHandler}
+                value={enteredTitle}
+              ></input>
+            </div>
+            <div className="new-book-control">
+              <label>Autor </label>
+              <input
+                type="text"
+                onChange={changeAuthorHandler}
+                value={enteredAuthor}
+              ></input>
+            </div>
+            <div className="new-book-control">
+              <label>Páginas </label>
+              <input
+                type="number"
+                min="1"
+                step="1"
+                onChange={changePageCountHandler}
+                value={enteredPageCount}
+              ></input>
+            </div>
+            <div className="new-book-control">
+              <label>¿Cuándo terminaste de leerlo? </label>
+              <input
+                type="date"
+                min="2019-01-01"
+                max="2022-12-31"
+                onChange={changeDateHandler}
+                value={enteredDate}
+              ></input>
+            </div>
+          </div>
+          <div className="new-book-actions">
+            <button type="submit">Agregar lectura</button>
+          </div>
+          <div className="new-book-actions">
+            <button type="button" onClick={() => setShowForm(false)}>
+              Cancelar
+            </button>
+          </div>
+        </form>
+      ) : (
+        <div className="new-book-actions">
+          <button type="button" onClick={() => setShowForm(true)}>
+            Agregar nueva lectura
+          </button>
         </div>
-        <div className="new-book-control">
-          <label>Autor </label>
-          <input
-            type="text"
-            onChange={changeAuthorHandler}
-            value={enteredAuthor}
-          ></input>
-        </div>
-        <div className="new-book-control">
-          <label>Páginas </label>
-          <input
-            type="number"
-            min="1"
-            step="1"
-            onChange={changePageCountHandler}
-            value={enteredPageCount}
-          ></input>
-        </div>
-        <div className="new-book-control">
-          <label>¿Cuándo terminaste de leerlo? </label>
-          <input
-            type="date"
-            min="2019-01-01"
-            max="2022-12-31"
-            onChange={changeDateHandler}
-            value={enteredDate}
-          ></input>
-        </div>
-      </div>
-      <div className="new-book-actions">
-        <button type="submit">Agregar lectura</button>
-      </div>
-    </form>
+      )}
+    </>
   );
 };
 
